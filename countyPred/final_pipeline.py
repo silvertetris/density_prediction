0a9a54411c092c665776c96bb51003c61738d266
sean <dltjdrnjs3@naver.com>
2025-09-03 20:34:31 +0900

    Update final_pipeline.py

--- countyPred/final_pipeline.py
+++ countyPred/final_pipeline.py
@@ -1,7 +1,8 @@
 from countyPred.cohort_calculate import calculate_with_last_index
+from countyPred.county_data_collector import county_data_collector
 from countyPred.data_err_calc import data_err_calc
 from countyPred.data_prex import data_prex
-from countyPred.geopandas_visualization import build_seoul_population_df, plot_seoul_choropleth
+from countyPred.geopandas_visualization import build_seoul_population_df, plot_seoul_choropleth, abstract_geojson
 from countyPred.xgBoost import forecast_all
 
 
@@ -11,16 +12,8 @@ def final_pipeline():
     #data_err_calc(result) #원본 err calc
     future_preds = forecast_all(result, n_lags=6, n_forecast=12) #xgBoost
     expected = calculate_with_last_index(result, future_preds) #cohort calc
-    seoul_df = build_seoul_population_df(shape_path=path)
-    plot_seoul_choropleth(
-        shape_path=path,
-        pop_df=seoul_df,
-        merge_on="sgg",  # GeoJSON & pop_df 공통 키
-        value_col="population",
-        filter_col=None,
-        filter_values=None,
-        cmap="OrRd",
-        title="서울: 1년 후 예측 인구"
-    )
+    #seoul_df = build_seoul_population_df(shape_path=path)
+    print(abstract_geojson(path).head(5))
+    wide_df = county_data_collector()
 
 final_pipeline()
